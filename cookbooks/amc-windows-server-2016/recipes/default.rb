b9b08876c4889b23a498ebcbc5105f2093fc4923
Tj Blogumas <ablogumas@amctheatres.com>
2018-08-15 17:44:57 -0500

    update readme with standardsd
    being plugging together cookbooks

--- cookbooks/amc-windows-server-2016/recipes/default.rb
+++ cookbooks/amc-windows-server-2016/recipes/default.rb
@@ -1,13 +1,9 @@
-# rubocop:disable EndOfLine
 # frozen_string_literal: true
 # This is a Chef recipe file. It can be used to specify resources which will
 # apply configuration to a server.
 
-log "Welcome to AMC SecureOS, #{node['amc']['secureos']['platform']}!" do
+log "Welcome to Chef, #{node['example']['name']}!" do
   level :info
 end
-
-include_recipe 'amc-windows-server-2016::default'
-
 
 # For more information, see the documentation: https://docs.chef.io/recipes.html
